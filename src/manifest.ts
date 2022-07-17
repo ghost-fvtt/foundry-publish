9dedd818855a709fb3ea9ae46622a8420881dd0a
Johannes Loher <johannes.loher@fg4f.de>
2022-07-17 17:11:03 +0200

    feat: add support for v10 manifests and maximum core version

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,9 +1,16 @@
 import * as t from 'io-ts';
 
+const PackageCompatibility = t.partial({
+  minimum: t.string,
+  verified: t.string,
+  maximum: t.string,
+});
+
 export const Manifest = t.partial({
   changelog: t.string,
   version: t.string,
   minimumCoreVersion: t.string,
   compatibleCoreVersion: t.string,
+  compatibility: PackageCompatibility,
   manifest: t.string,
 });
